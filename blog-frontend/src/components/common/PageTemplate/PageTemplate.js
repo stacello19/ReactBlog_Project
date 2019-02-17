0abaaf243559c2eab4744a583f72c1775556b474
Stacy Cho <stacello19@gmail.com>
2019-02-17 12:31:20 -0500

    Header and footer

---
+++ blog-frontend/src/components/common/PageTemplate/PageTemplate.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './PageTemplate.scss';
+import classNames from 'classnames/bind';
+import Header from 'components/common/Header';
+import Footer from 'components/common/Footer';
+
+const cx = classNames.bind(styles);
+
+const PageTemplate = () => (
+  <div className={cx('page-template')}>
+     <Header/>
+     <Footer/>
+  </div>
+);
+
+export default PageTemplate;
