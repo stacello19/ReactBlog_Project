6481baa49dff395cefbb35fbf5555b92fec6f77b
Stacy Cho <stacello19@gmail.com>
2019-02-18 22:12:57 -0500

    PostInfo

--- blog-frontend/src/components/App.js
+++ blog-frontend/src/components/App.js
@@ -8,7 +8,8 @@ const App = () => {
             <Switch>
                 <Route exact path="/" component={ListPage}/>
                 <Route path="/page/:page" component={ListPage}/>
-                <Route path="/tag/:tag/:page?" component={PostPage}/>
+                <Route path="/tag/:tag/:page?" component={ListPage}/>
+                <Route path="/post/:id" component={PostPage}/>
                 <Route path="/editor" component={EditorPage}/>
                 <Route component={NotFoundPage}/>
             </Switch>
