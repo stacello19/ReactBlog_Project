095ef6c74e852621cda18b1eeb4b9428c1d37279
Stacy Cho <stacello19@gmail.com>
2019-02-18 22:57:53 -0500

    EditorPane 1st

--- blog-frontend/src/pages/EditorPage.js
+++ blog-frontend/src/pages/EditorPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import EditorTemplate from 'components/editor/EditorTemplate';
 import EditorHeader from 'components/editor/EditorHeader';
+import EditorPane from 'components/editor/EditorPane';
 
 const EditorPage = () => {
     return (
         <EditorTemplate
             header={<EditorHeader/>}
-            editor='editor'
+            editor={<EditorPane/>}
             preview='preview'
         />
     );
