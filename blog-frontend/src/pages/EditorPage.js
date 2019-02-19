0fa1882479c069b34659f98d863731aaf84e28c6
Stacy Cho <stacello19@gmail.com>
2019-02-18 22:31:33 -0500

    editor beginning

--- blog-frontend/src/pages/EditorPage.js
+++ blog-frontend/src/pages/EditorPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import EditorTemplate from 'components/editor/EditorTemplate';
 
 const EditorPage = () => {
     return (
-        <div>
-            Editor
-        </div>
+        <EditorTemplate
+            header='header'
+            editor='editor'
+            preview='preview'
+        />
     );
 };
 
