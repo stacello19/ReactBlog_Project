0c8bad57ffa2f886adc2d9180e584e8474782481
Stacy Cho <stacello19@gmail.com>
2019-02-20 12:30:23 -0500

    codemirror works!

--- blog-frontend/src/components/editor/EditorPane/EditorPane.js
+++ blog-frontend/src/components/editor/EditorPane/EditorPane.js
@@ -1,15 +1,40 @@
 import React, {Component} from 'react';
 import styles from './EditorPane.scss';
 import classNames from 'classnames/bind';
+import CodeMirror from 'codemirror';
+import 'codemirror/mode/markdown/markdown';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/jsx/jsx';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/shell/shell';
+
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
 
 const cx = classNames.bind(styles);
 
 class EditorPane extends Component {
+    editor = null
+    codeMirror = null
+
+    initializeEditor = () => {
+        this.codeMirror = CodeMirror(this.editor, {
+            mode: 'markdown',
+            theme: 'monokai',
+            // 문제야
+            lineNumbers: false,
+            lineWrapping: true
+        })
+    }
+    componentDidMount(){
+        this.initializeEditor();
+    }
+
     render () {
         return(
             <div className={cx('editor-pane')}>
                 <input className={cx('title')} placeholder='제목을 입력하세요' name="title"/>
-                <div className={cx('code-editor')}></div>
+                <div className={cx('code-editor')} ref={ref => this.editor = ref}></div>
                 <div className={cx('tags')}>
                     <div className={cx('description')}>태그</div>
                     <input name="tags" placeholder="태그를 입력하세요 (쉼표로 구분)"/>
