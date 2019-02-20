147f1075030dc4692a6334666a50c88111215777
Stacy Cho <stacello19@gmail.com>
2019-02-20 18:56:30 -0500

    animate button

--- blog-frontend/src/components/modal/ModalWrapper/ModalWrapper.js
+++ blog-frontend/src/components/modal/ModalWrapper/ModalWrapper.js
@@ -5,14 +5,35 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 class ModalWrapper extends Component {
+    state = {
+        animate: false
+    }
+    startAnimation = () => {
+        this.setState({
+            animate: true
+        });
+        setTimeout(() => {
+            this.setState({
+                animate: false
+            })
+        }, 250)
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if(prevProps.visible !== this.props.visible) {
+            this.startAnimation();
+        }
+    }
     render() {
         const { children, visible } = this.props;
-        if(!visible) return null;
+        const { animate } = this.state;
+        if(!visible && !animate) return null;
+
+        const animation = animate && (visible ? 'enter' : 'leave');
         return (
             <div>
                 <div className={cx('gray-background')}/>
                 <div className={cx('modal-wrapper')}>
-                    <div className={cx('modal')}>
+                    <div className={cx('modal', animation)}>
                     {children}
                     </div>
                 </div>
